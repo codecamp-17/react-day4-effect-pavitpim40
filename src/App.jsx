a565e820970b0165c7df76b9bd7d56389e9ecc2f
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-03 16:46:40 +0700

    debouncing search

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 // #1 Basic CleanUp
 // let count = 0;
@@ -19,30 +20,74 @@ import { useEffect, useState } from 'react';
 //   return <input className='border-2' value={data} onChange={(e) => setData(e.target.value)} />;
 // }
 
-// ## Component Will Unmount (ก่อนตาย)
+// ## 2 Component Will Unmount (ก่อนตาย)
 
-function Input() {
-  // Outer of Outer
-  const [data, setData] = useState('');
-  useEffect(() => {
-    // Outer Scope
-    console.log('I was born');
-    return () => {
-      // Inner Scope
-      console.log("I'm gonna die with", data); // "" , Closure of 1st Render
-    };
-  }, []);
-  return <input className='border-2' value={data} onChange={(e) => setData(e.target.value)} />;
-}
+// function Input() {
+//   // Outer of Outer
+//   const [data, setData] = useState('');
+//   useEffect(() => {
+//     // Outer Scope
+//     console.log('I was born');
+//     return () => {
+//       // Inner Scope
+//       console.log("I'm gonna die with", data); // "" , Closure of 1st Render
+//     };
+//   }, []);
+//   return <input className='border-2' value={data} onChange={(e) => setData(e.target.value)} />;
+// }
 
+// function App() {
+//   const [isShow, setIsShow] = useState(false);
+//   return (
+//     <div>
+//       <button className='bg-slate-500 p-1 text-white' onClick={() => setIsShow(!isShow)}>
+//         Toggle
+//       </button>
+//       {isShow && <Input />}
+//     </div>
+//   );
+// }
+
+// ## 3 : Multiple Request
+let count = 0;
 function App() {
-  const [isShow, setIsShow] = useState(false);
+  const [data, setData] = useState('');
+  const [users, setUser] = useState([]);
+
+  // Multi Request
+  // useEffect(() => {
+  //   searchUser();
+  //   count++;
+  // }, [data]);
+
+  // Fixed : Debounce
+  useEffect(() => {
+    let registerId = setTimeout(() => {
+      searchUser();
+    }, 3000);
+    console.log('ID', registerId);
+    return () => {
+      console.log('ID-CleanUp', registerId);
+      clearTimeout(registerId);
+    };
+  }, [data]);
+
+  const searchUser = async () => {
+    try {
+      let response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setUser(response.data);
+      console.log('APIs ', count, response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <button className='bg-slate-500 p-1 text-white' onClick={() => setIsShow(!isShow)}>
-        Toggle
-      </button>
-      {isShow && <Input />}
+      <input
+        className='bg-black text-white'
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+      />
     </div>
   );
 }
