f9a35b65c6cc570c9692cb13f8ef6a1e889ca29b
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-06 09:24:00 +0700

    finish:1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-function App() {
-  return <h1>Hello world!</h1>;
+import React, { useEffect } from 'react';
+
+// Function Component
+export default function App() {
+  // ### START -- FN BODY (Run Before Every Render)
+
+  // ###### START USE-EFFECT (Run After Every Render)
+  React.useEffect(() => {
+    alert('After First Render');
+  }, []);
+  // ###### END USE-EFFECT
+
+  alert('Before Render');
+
+  // ### END -- FN BODY
+  // ### START -- FN RETURN
+  return <main>Hello, React</main>;
 }
 
-export default App;
+// FN-BODY -> Execute Code -> FN-RETURN -> Render -> Execute useEffect
