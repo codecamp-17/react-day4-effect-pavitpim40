17d8a876192188a4de1f50d083aaa3230f568f29
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-06 09:58:42 +0700

    finish:2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export default function App() {
-  const [email, setEmail] = useState('');
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    if (email.includes('@')) setIsError(false);
-    else setIsError(true);
-  }, [email]);
+  const [user, setUser] = useState(null);
+  const fetchUser = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      console.log(error);
+    }
+    // task-1 เขียนโค้ดสำหรับ Fetch user จาก https://jsonplaceholder.typicode.com/users/1
+    // task-3 นำข้อมูลที่ได้จากการ Fetch มาอัพเดท user state
+    // task-2 นำฟังก์ชัน fetchUser ไป binding กับ ปุ่ม
+    // task-4 ลองดูผลลัพธ์ผ่าน browser + อธิบาย code ให้ตัวเองเข้าใจ
+  };
   return (
     <main>
-      <input
-        type='email'
-        className='border-2'
-        style={{ borderColor: isError ? 'red' : 'green' }}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+      {user ? (
+        <>
+          <h1>name : {user.name}</h1>
+          <h2>email : {user.email}</h2>
+        </>
+      ) : (
+        <h1>Loading.....</h1>
+      )}
+      <button className='bg-slate-500 p-1 rounded-md text-white' onClick={fetchUser}>
+        fetch user
+      </button>
     </main>
   );
 }
